46ba31168d5a9c1048c79804edfab946aadd4f48
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2018-06-21 15:16:17 +0000

    FR 1161 Display title year on 'Most-viewed' title reports
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@157 c56fabd3-1acd-401a-a842-f99f0872de9c

--- biblio/most_viewed.py
+++ biblio/most_viewed.py
@@ -1,6 +1,6 @@
 #!_PYTHONLOC
 #
-#     (C) COPYRIGHT 2013-2016   Ahasuerus
+#     (C) COPYRIGHT 2013-2018   Ahasuerus
 #       ALL RIGHTS RESERVED
 #
 #     The copyright notice above does not evidence any actual or
@@ -20,11 +20,15 @@ def printTable(type):
 	print '<th>Rank</th>'
 	print '<th>Views</th>'
 	print '<th>Title</th>'
+	print '<th>Year</th>'
 	print '<th>Authors</th>'
 	print '</tr>'
 
-        query = "select @rownum:=@rownum+1, title_views, title_id, title_title from titles, "
-        query += "(SELECT @rownum:=0) as r where title_ttype='%s' order by title_views desc limit 300" % (db.escape_string(type))
+        query = """select @rownum:=@rownum+1, title_views, title_id, title_title, title_copyright
+                from titles, (SELECT @rownum:=0) as r
+                where title_ttype='%s'
+                order by title_views
+                desc limit 500""" % (db.escape_string(type))
 
 	db.query(query)
 	result = db.store_result()
@@ -36,10 +40,12 @@ def printTable(type):
                 views = record[0][1]
                 title_id = record[0][2]
                 title_title = record[0][3]
+                title_year = convertYear(record[0][4])
                 print '<tr align=left class="table%d">' % (bgcolor+1)
                 print '<td>%d</d>' % rank
                 print '<td>%d</td>' % views
                 print '<td>%s</td>' % ISFDBLink('title.cgi', title_id, title_title)
+                print '<td>%s</td>' % title_year
                 print '<td>'
                 PrintAllAuthors(title_id)
                 print '</td>'
@@ -50,32 +56,36 @@ def printTable(type):
         
 
 if __name__ == '__main__':
-        if len(sys.argv) < 2:
-                PrintHeader("Most-Viewed Data Since 2005")
+
+        try:
+                report_type = sys.argv[1]
+                if report_type not in ('authors', 'novels', 'short'):
+                        raise
+        except:
+                PrintHeader('Most-Viewed Data Since 2005')
                 PrintNavbar('top', 0, 0, 'most_viewed.cgi', 0)
-                print "<h3>No parameter passed</h3>"
+                print '<h3>Invalid parameter</h3>'
                 sys.exit(0)
 
-        if sys.argv[1] == 'authors':
-                query = "select @rownum:=@rownum+1, author_canonical, author_views, author_id from authors, (SELECT @rownum:=0) as r order by author_views desc limit 500"
+        if report_type == 'authors':
+                query = """select @rownum:=@rownum+1, author_canonical, author_views, author_id
+                from authors, (SELECT @rownum:=0) as r
+                order by author_views desc
+                limit 500"""
                 headers = ('Rank', 'Views', 'Author')
                 PrintListPage('Most Viewed Authors Since 2005', 'most_viewed', 0, 0, 'most_viewed.cgi', 0, query, AuthorsDisplayFunc, headers)
                 sys.exit(0)
 
-        if sys.argv[1] == 'novels':
+        if report_type == 'novels':
                 header = 'Novels'
-        elif sys.argv[1] == 'short':
-                header = 'Short Fiction'
         else:
-                header = 'Data'
+                header = 'Short Fiction'
 
-	PrintHeader("Most-Viewed "+ header + " Since 2005")
+	PrintHeader("Most-Viewed %s Since 2005" % header)
 	PrintNavbar('top', 0, 0, 'most_viewed.cgi', 0)
 
-        if sys.argv[1] == 'novels':
+        if report_type == 'novels':
                 printTable('NOVEL')
-        elif sys.argv[1] == 'short':
-                printTable('SHORTFICTION')
         else:
-                print '<h3>Bad Argument</h3>'
+                printTable('SHORTFICTION')
 	PrintTrailer('top', 0, 0)
