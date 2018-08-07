3947e0798facc46f7c3bc3eab95669d1fe6ac1cf
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2018-08-07 15:04:46 +0000

    SR 136 Store statistical reports in the database as opposed to OS files - Part 3 - Eliminate LOCALFILES
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@182 c56fabd3-1acd-401a-a842-f99f0872de9c

--- common/localdefs.py
+++ common/localdefs.py
@@ -1,5 +1,5 @@
 #
-#     (C) COPYRIGHT 2005-2013   Al von Ruff, Bill Longley and Ahasuerus
+#     (C) COPYRIGHT 2005-2018   Al von Ruff, Bill Longley and Ahasuerus
 #       ALL RIGHTS RESERVED
 #
 #     The copyright notice above does not evidence any actual or
@@ -14,7 +14,6 @@ HTFAKE	   	= "/www.isfdb.org/cgi-bin"
 DBASEHOST  	= "localhost"
 HTMLHOST   	= "www.isfdb.org"
 COOKIEHOST   	= "www.isfdb.org"
-LOCALFILES      = "/var/www/html/"
 WIKILOC         = "www.isfdb.org/wiki"
 USERNAME   	= "root"
 #PASSWORD   	= "XXXXXX"
