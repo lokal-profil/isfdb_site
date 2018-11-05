8d2fbc536c760e541cbe50dcc4a277d0b29ab117
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2018-11-05 20:08:35 +0000

    SR 114 Improve the performance of cleanup reports - Part 14 - Filer changes
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@247 c56fabd3-1acd-401a-a842-f99f0872de9c

--- nightly/nightly_lib.py
+++ nightly/nightly_lib.py
@@ -17,13 +17,14 @@ class elapsedTime:
                 self.start = time()
                 # Save the system stdout
                 self.stdout = sys.stdout
+                # Only print a message if the execution time of a report exceeds this threshold
+                self.threshold = 2
 
-        def print_elapsed(self, report_name, threshold = 2):
+        def print_elapsed(self, report_name):
                 sys.stdout = self.stdout
-                # If the elapsed time is more than 1 second, print how long it took to compile this report
                 elapsed = time() - self.start
-                if elapsed > threshold:
-                        print '%s took %.2f seconds to compile' % (report_name, elapsed)
+                if elapsed > self.threshold:
+                        print '%s: Rpt %s: %.2f seconds' % (strftime('%H:%M:%S', localtime()), report_name, elapsed)
                 self.start = time()
 
 
@@ -47,11 +48,18 @@ def standardReportFromList(id_list, report_type):
         elapsed.print_elapsed(report_type)
 
 def standardReportInsert(id_list, report_type):
+        # First remove previously resolved/ignored records from the list of IDs
+        query = "select record_id from cleanup where report_type=%d and resolved=1" % int(report_type)
+        db.query(query)
+	result = db.store_result()
+        record = result.fetch_row()
+	while record:
+		resolved_id = record[0][0]
+		if resolved_id in id_list:
+                        id_list.remove(resolved_id)
+        	record = result.fetch_row()
+
+        # Next add the new IDs to the cleanup table
         for record_id in id_list:
-                query = "select 1 from cleanup where report_type=%d and record_id=%d and resolved=1" % (int(report_type), int(record_id))
-                db.query(query)
-                result = db.store_result()
-                # Only add to the cleanup table if this record is not "resolved"
-                if not result.num_rows():
-                        update = "insert into cleanup (record_id, report_type) values(%d, %d)" % (int(record_id), int(report_type))
-                        db.query(update)
+                update = "insert into cleanup (record_id, report_type) values(%d, %d)" % (int(record_id), int(report_type))
+                db.query(update)
