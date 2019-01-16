4b04621f343250213d2b1ec87fcc334b8f029781
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2019-01-16 20:00:40 +0000

    FR 1258 Add 'Fantascienza' to the NILF description
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@325 c56fabd3-1acd-401a-a842-f99f0872de9c

---
+++ scripts/update_NILF_full_name.sql
@@ -0,0 +1,17 @@
+/* 
+   update_NILF_full_name.sql is a MySQL script intended to update of the NILF
+   external idenifier type to include 'Fantascienza'
+	
+
+   Version: $Revision: 15 $
+   Date:    $Date: 2018-06-13 16:32:38 -0400 (Tue, 31 Oct 2017) $
+
+  (C) COPYRIGHT 2019 Ahasuerus
+      ALL RIGHTS RESERVED
+
+  The copyright notice above does not evidence any actual or
+  intended publication of such source code.
+*/
+
+
+UPDATE identifier_types set identifier_type_full_name = 'Numero Identificativo della Letteratura Fantastica / Fantascienza' where identifier_type_name = ''NILF'';
