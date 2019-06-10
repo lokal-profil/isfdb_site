d271259dd609dc814b5a9c92cf5ae4db7c31eecc
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2019-06-10 23:00:02 +0000

    SR 171 Remove obsolete 'missing URL' processing - Dropped the table with the missing URLs
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@425 c56fabd3-1acd-401a-a842-f99f0872de9c

---
+++ scripts/drop_missing_author_urls.sql
@@ -0,0 +1,14 @@
+/* 
+   drop_missing_author_urls.sql is a MySQL script intended to
+   drop the table missing_author_urls
+
+   Version: $Revision: 418 $
+   Date:    $Date: 2019-05-15 10:10:07 -0400 (Wed, 15 May 2019) $
+
+  (C) COPYRIGHT 2019 Ahasuerus
+      ALL RIGHTS RESERVED
+
+  The copyright notice above does not evidence any actual or
+  intended publication of such source code.
+*/
+drop table missing_author_urls;
