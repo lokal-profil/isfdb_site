efa5a4c11f745c0491aa1c64bc613e8b58a6a97a
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2019-08-27 20:13:45 +0000

    FR 1296 Add the Maithili language
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@455 c56fabd3-1acd-401a-a842-f99f0872de9c

---
+++ scripts/add_maithili_language.sql
@@ -0,0 +1,15 @@
+/* 
+   add_maithili_language.sql is a MySQL script intended to add
+   the Maithili language to ISFDB
+
+   Version: $Revision: 418 $
+   Date:    $Date: 2019-05-15 10:10:07 -0400 (Wed, 15 May 2019) $
+
+  (C) COPYRIGHT 2019 Ahasuerus
+      ALL RIGHTS RESERVED
+
+  The copyright notice above does not evidence any actual or
+  intended publication of such source code.
+*/
+
+INSERT INTO languages (lang_id, lang_code, lang_name) VALUES (155, 'mai', 'Maithili');
