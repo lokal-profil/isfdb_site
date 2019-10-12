7fddda67b3a4babfdb0f30d2afdd14dbe614530d
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2019-10-12 23:53:45 +0000

    FR 1309 Create an 'Incomplete' template
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@463 c56fabd3-1acd-401a-a842-f99f0872de9c

--- common/library.py
+++ common/library.py
@@ -770,6 +770,7 @@ def FormatNote(note, note_type = '', display_mode = 'short', record_id = 0, reco
                 'FantLab-author': ('https://fantlab.ru/autor%s', 'FantLab author'),
                 'FantLab-pub': ('https://fantlab.ru/edition%s', 'FantLab publication'),
                 'FantLab-title': ('https://fantlab.ru/work%s', 'FantLab title'),
+                'Incomplete': ('', 'Contents incomplete.', ),
                 'ISBN': ('%s', 'Additional ISBN'),
                 'JNB': ('https://iss.ndl.go.jp/api/openurl?ndl_jpno=%s&locale=en', 'JNB',
                         'Japanese National Bibliography'),
