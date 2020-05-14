0d8f404fdea979b86ded9f7b1e16c6a50b05a9b8
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2020-05-14 23:47:06 +0000

    FR 1349 Remove 'unknown' from the list of marque authors
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@532 c56fabd3-1acd-401a-a842-f99f0872de9c

--- mod/marque.py
+++ mod/marque.py
@@ -1,6 +1,6 @@
 #!_PYTHONLOC
 #
-#     (C) COPYRIGHT 2005-2006   Al von Ruff
+#     (C) COPYRIGHT 2005-2020   Al von Ruff and Ahasuerus
 #       ALL RIGHTS RESERVED
 #
 #     The copyright notice above does not evidence any actual or
@@ -26,27 +26,32 @@ if __name__ == '__main__':
 	#################################
 	# Calculate top 2 percent
 	#################################
-        query = "select count(author_id) from authors;"
+        query = 'select count(author_id) from authors'
         db.query(query)
         result = db.store_result()
         record = result.fetch_row()
 	total_authors = record[0][0]
 	maxauthors = total_authors / 50
 
-	update = "update authors set author_marque='0'"
+	update = 'update authors set author_marque = 0'
 	db.query(update)
 
-	query = "select author_id,author_views,author_canonical from authors where author_views > 0 order by author_views desc limit %d" % maxauthors
+	query = """select author_id, author_views, author_canonical
+                from authors
+                where author_views > 0
+                and author_canonical != 'unknown'
+                order by author_views
+                desc limit %d""" % maxauthors
 	db.query(query)
 	result = db.store_result()
 	record = result.fetch_row()
-	print "<ol>"
+	print '<ol>'
 
 	while record:
-		print "<li>%d - %s" % (record[0][1], record[0][2])
-		update = "update authors set author_marque='1' where author_id=%s" % (record[0][0])
+		print '<li>%d - %s' % (record[0][1], record[0][2])
+		update = 'update authors set author_marque=1 where author_id=%d' % int(record[0][0])
 		db.query(update)
 		record = result.fetch_row()
 
-	print "</ol>"
-	PrintPostMod()
+	print '</ol>'
+	PrintPostMod(0)
