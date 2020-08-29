16ea9760b8f5bd752a3358922286c5ae3c160f1e
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2020-08-29 23:07:35 +0000

    FR 1368 Add support for jeddicon.com-hosted images
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@551 c56fabd3-1acd-401a-a842-f99f0872de9c

--- common/library.py
+++ common/library.py
@@ -1007,6 +1007,7 @@ def RecognizedDomains():
                 ('instagram.com', 'Instagram', 'instagram.com', 0),
                 ('isfdb.org', 'ISFDB', 'www.isfdb.org', 1),
                 ('ivy-bells.com', 'Dr. Robert G. Williscroft', 'ivy-bells.com', 1),
+                ('jeddicon.com', 'JeddiCon', 'jeddicon.com', 1),
                 ('jenniferrpovey.com', 'Jennifer R. Povey', 'www.jenniferrpovey.com', 1),
                 ('kirjasto.sci.fi', 'Books and Writers', 'kirjasto.sci.fi', 0),
                 ('librarything.com', 'LibraryThing', 'librarything.com', 0),
