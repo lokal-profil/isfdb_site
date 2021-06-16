597f8da297672b02c71e0b36c82ddcf0280f56ab
ahasuerus_isfdb <ahasuerus_isfdb@c56fabd3-1acd-401a-a842-f99f0872de9c>
2021-06-16 19:03:58 +0000

    SR 185 Remove vstats.cgi - Part 1
    git-svn-id: svn://svn.code.sf.net/p/isfdb/code-svn/trunk@636 c56fabd3-1acd-401a-a842-f99f0872de9c

--- biblio/vstats.py
+++ biblio/vstats.py
@@ -1,6 +1,6 @@
 #!_PYTHONLOC
 #
-#     (C) COPYRIGHT 2005-2016   Al von Ruff and Ahasuerus
+#     (C) COPYRIGHT 2005-2021   Al von Ruff and Ahasuerus
 #       ALL RIGHTS RESERVED
 #
 #     The copyright notice above does not evidence any actual or
@@ -10,23 +10,16 @@
 #     Date: $Date$
 
 
-import sys
-import os
-import string
 from SQLparsing import *
 from common import *
 
-limit = 300
 
 if __name__ == '__main__':
 
+        year = SESSION.Parameter(0, 'int', 0)
+
 	PrintHeader('Votes for Titles with More Than 5 Votes')
 	PrintNavbar('stats', 0, 0, 'vstats.cgi', 0)
-
-	try:
-		year = int(sys.argv[1])
-	except:
-		year = 0
 
 	print '<ol>'
 
@@ -43,4 +36,4 @@ if __name__ == '__main__':
 
 	print '</ol>'
 
-	PrintTrailer('frontpage', 0, 0)
+	PrintTrailer('stats', 0, 0)
