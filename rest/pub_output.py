b9eb9c01c8b1ca9c348dd1f3e9313608ed0fe732
lokal-profil <lokal.profil@gmail.com>
2022-03-13 22:12:22 +0100

    Standardise indent and refactor xml builder

--- rest/pub_output.py
+++ rest/pub_output.py
@@ -18,34 +18,35 @@ from pubClass import pubs
 
 
 def pubOutput(pub_bodies):
-	print '<?xml version="1.0" encoding="iso-8859-1" ?>'
-	print '<ISFDB>'
-	print '  <Records>%d</Records>' % len(pub_bodies)
-	print '  <Publications>'
-	for pub_body in pub_bodies:
-		onePubOutput(pub_body)
-	print '  </Publications>'
-	print '</ISFDB>'
+        print '<?xml version="1.0" encoding="iso-8859-1" ?>'
+        print '<ISFDB>'
+        printTag(len(pub_bodies), 'Records', indent=1)
+        print '  <Publications>'
+        for pub_body in pub_bodies:
+                print '    <Publication>'
+                onePubOutput(pub_body)
+                print '    </Publication>'
+        print '  </Publications>'
+        print '</ISFDB>'
 
-def onePubOutput(publication):	
+def onePubOutput(publication):
 
         pub = pubs(db)
         pub.pub_id = publication[PUB_PUBID]
 
-	print '    <Publication>'
-	print '      <Record>%s</Record>' % pub.pub_id
-	print '      <Title>%s</Title>' % XMLescape(publication[PUB_TITLE], 1)
-	authors = SQLPubAuthors(pub.pub_id)
-	printMultiValued(authors, 'Authors', 'Author')
+        printTag(pub.pub_id, 'Record')
+        printTag(publication[PUB_TITLE], Title)
+        authors = SQLPubAuthors(pub.pub_id)
+        printMultiValued(authors, 'Authors', 'Author')
 
-	if publication[PUB_YEAR]:
+        if publication[PUB_YEAR]:
                 printTag(convertDate(publication[PUB_YEAR], 1), 'Year')
-	printTag(publication[PUB_ISBN], 'Isbn')
-	printTag(publication[PUB_CATALOG], 'Catalog')
-	if publication[PUB_PUBLISHER]:
-		publisher = SQLGetPublisher(publication[PUB_PUBLISHER])
-		printTag(publisher[PUBLISHER_NAME], 'Publisher')
-	if publication[PUB_SERIES]:
+        printTag(publication[PUB_ISBN], 'Isbn')
+        printTag(publication[PUB_CATALOG], 'Catalog')
+        if publication[PUB_PUBLISHER]:
+                publisher = SQLGetPublisher(publication[PUB_PUBLISHER])
+                printTag(publisher[PUBLISHER_NAME], 'Publisher')
+        if publication[PUB_SERIES]:
                 pubseries = SQLGetPubSeries(publication[PUB_SERIES])
                 printTag(pubseries[PUB_SERIES_NAME], 'PubSeries')
         printTag(publication[PUB_SERIES_NUM], 'PubSeriesNum')
@@ -56,33 +57,37 @@ def onePubOutput(publication):
         printTag(publication[PUB_TAG], 'Tag')
         printTag(publication[PUB_IMAGE], 'Image')
 
-	artists = SQLPubArtists(pub.pub_id)
+        artists = SQLPubArtists(pub.pub_id)
         printMultiValued(artists, 'CoverArtists', 'Artist')
 
-	if publication[PUB_NOTE]:
-		notes = SQLgetNotes(publication[PUB_NOTE])
-		printTag(notes, 'Note')
+        if publication[PUB_NOTE]:
+                notes = SQLgetNotes(publication[PUB_NOTE])
+                printTag(notes, 'Note')
 
         pub.loadExternalIDs()
         if pub.identifiers:
                 print pub.xmlIdentifiers(1)
-        
+
         pub.loadPubWebpages()
         printMultiValued(pub.pub_webpages, 'Webpages', 'Webpage')
-        
+
         pub.loadTransTitles()
         printMultiValued(pub.pub_trans_titles, 'TransTitles', 'TransTitle')
-	print '    </Publication>'
 
-def printTag(value, tag):
+def printTag(value, tag, indent=3, linebreak=False):
         if not value:
                 return
-        print '      <%s>%s</%s>' % (tag, XMLescape(value, 1), tag)
+        if linebreak:
+                internal = '\n%s%s\n%s' % ('  ' * (indent + 1), XMLescape(value, 1),'  ' * indent)
+        else:
+                internal = XMLescape(value, 1)
+        print '%s<%s>%s</%s>' % ('  ' * indent, tag, internal, tag)
 
-def printMultiValued(values, outer_tag, inner_tag):
+def printMultiValued(values, outer_tag, inner_tag, indent=3):
         if not values:
                 return
-        print '      <%s>' % outer_tag
+        output = '%s<%s>\n' % ('  ' * indent, outer_tag)
         for value in values:
-                print '        <%s>%s</%s>' % (inner_tag, XMLescape(value, 1), inner_tag)
-        print '      </%s>' % outer_tag
+                output += '%s<%s>%s</%s>\n' % ('  ' * (indent + 1), inner_tag, XMLescape(value, 1), inner_tag)
+        output += '%s</%s>\n' % ('  ' * indent, outer_tag)
+        print output
